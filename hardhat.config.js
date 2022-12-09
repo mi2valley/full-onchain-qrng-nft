029f36a9ff9358daf5e72ade4ae29deb5ae3122e
mi2valley <monocoin189@gmail.com>
2022-12-09 23:43:47 +0900

    fix byog

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,16 +3,17 @@ require("hardhat-deploy");
 require("dotenv").config();
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 const MATIC_PRIVATE_KEY = process.env.MATIC_PRIVATE_KEY;
+const GOERLI_API_KEY = process.env.GOERLI_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${GOERLI_API_KEY}`,
+      accounts: [`0x${GOERLI_PRIVATE_KEY}`],
     },
     matic: {
       url: "https://rpc-mainnet.maticvigil.com/",
